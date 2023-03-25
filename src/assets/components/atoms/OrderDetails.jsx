ca12a4eebe65655aba27af836f274184c8b75df7
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-03-25 11:45:47 +0100

    complete OrderDetails component

--- src/assets/components/atoms/OrderDetails.jsx
+++ src/assets/components/atoms/OrderDetails.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import './orderDetails.css'
 
-function OrderDetails() {
-    return (
-      <>
+function OrderDetails(props) {
+    const { deliveryDate, pickupDate } = props;
+    
+  return (
+    <div className="oderDetailsContainer">
         <div>
-            
+            <h2>Parcel List {deliveryDate}</h2>
+            <p>5 carriers picked up the parcel on {pickupDate}</p>
+            <p>14 items</p>
         </div>
-      </>
-    );
-  }
-  
-  export default OrderDetails;
+        <div>
+            <p className="deliveryDate">{deliveryDate}</p>
+        </div>
+    </div>
+  );
+}
+
+export default OrderDetails;
--- src/assets/pages/ParcelLists.jsx
+++ src/assets/pages/ParcelLists.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React from 'react';
 import OrderDetails from '../components/atoms/OrderDetails';
+import data from './data.json';
 
 function ParcelLists() {
-    return (
-      <>
-      <h1>Parcel Lists</h1>
-        <div>
-            <OrderDetails/>
-        </div>
-      </>
-    );
-  }
-  
-  export default ParcelLists;
+
+  const returnData = data.slice(0, 15);
+
+  return (
+    <>
+    <h1>Parcel Lists</h1>
+    <div>
+      {returnData.map((parcel) => (
+        <OrderDetails
+          key={parcel.id}
+          deliveryDate={parcel.deliveryDate}
+          pickupDate={parcel.pickupDate}
+        />
+      ))}
+    </div>
+    </>
+  );
+}
+
+export default ParcelLists;
+
+
+
+
+// function ParcelLists() {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     axios
+//       .get('https://challenges-asset-files.s3.us-east-2.amazonaws.com/Events/Media+Markt/Challenges/parcels_mm.json/{id}')
+//       .then(response => {
+//         setData(response.data);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   }, []);
+
+//   if (!data) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       {data.map(parcel => (
+//         <OrderDetails key={parcel.id.$oid} pickupDate={parcel.pickupDate} deliveryDate={parcel.deliveryDate} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default ParcelLists;
