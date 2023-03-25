bf143239290adadb0c85266fdf203f22ef082e12
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-03-25 10:12:43 +0100

    add page ParcelLists, add component OrderDetails, install axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router"
+import ParcelLists from './assets/pages/ParcelLists';
 
 function App() {
   return (
     <>
+    <Routes>
+        <Route path="/" element={<ParcelLists/>} />
+      </Routes>
     </>
   );
 }
---
+++ src/assets/pages/ParcelLists.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import OrderDetails from '../components/atoms/OrderDetails';
+
+function ParcelLists() {
+    return (
+      <>
+      <h1>Parcel Lists</h1>
+        <div>
+            <OrderDetails/>
+        </div>
+      </>
+    );
+  }
+  
+  export default ParcelLists;
